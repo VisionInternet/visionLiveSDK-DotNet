fdeefddfe78cda6e7ffd37ae0108762c906e2cc1
dingyuliang <dingyuliang@msn.com>
2017-06-09 23:08:54 -0700

    Checked in Example Project

---
+++ example/Vision.Api.Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vision.Api.Common;
+using Vision.Cms.MvcComponents.Calendar.Api.Request;
+using Vision.Cms.MvcComponents.Calendar.Api.Response;
+
+namespace Vision.Api.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new JSONApiClient("http://www.city.gov/API", "*********", "*************");
+            var request = new EventGetRequest();
+            request.ID = 1;
+            request.Fields = 1;
+            var response = client.Execute<EventGetRequest, EventGetResponse>(request);
+        }
+    }
+}
